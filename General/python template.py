36006d037d77546c1c1a15add04b63ea22c866f8
Feyiron <45174056+Feyiron@users.noreply.github.com>
2022-12-01 10:14:55 +0100

    reorganised

---
+++ General/python template.py
@@ -0,0 +1,12 @@
+# get input from file
+inputfile = open("C:\\GitHub\\aoc-2022\\Day X\\inputdayx.txt", "r")
+
+# go over input file
+for line in inputfile:
+    # do something dumb
+
+######## Part 1 ########
+print("Part 1: " + )
+
+######## Part 2 ########
+print("Part 2: " + )
